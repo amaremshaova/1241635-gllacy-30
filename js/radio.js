37fc581de7f0290b1fa9ed20c79d34628aa00c95
maremshaowa.nasti@mail.ru <maremshaowa.nasti@mail.ru>
2021-03-17 23:01:06 +0300

    Исправление radio

--- js/radio.js
+++ js/radio.js
@@ -3,6 +3,6 @@ const fatFilterItemList = document.querySelectorAll(".fat-content-filter-item");
 fatFilterItemList.forEach(el =>
     el.childNodes[3].addEventListener('keydown', function(event) {
         if (event.keyCode == 13)
-        el.childNodes[1].checked = 'checked';
+        el.childNodes[1].checked = true;
     })
 ); 
