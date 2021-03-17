ab2e4339bacfd4e6778654e189ea855df7cb2dfb
maremshaowa.nasti@mail.ru <maremshaowa.nasti@mail.ru>
2021-03-17 14:56:19 +0300

    Исправление замечаний по критериям

--- js/login.js
+++ js/login.js
@@ -1,27 +1,27 @@
-const authorizationEmailUser = document.querySelector('.authorization-login-input'); 
-const authorizationPasswordUser = document.querySelector('.authorization-password-input'); 
-const authorizationButton = document.querySelector('.authorization-button'); 
-const authorizationForm = document.querySelector('.authorization-form');
-const authorizationSection = document.querySelector('.authorization-section'); 
+const authorizationEmailUser = document.querySelector(".authorization-login-input"); 
+const authorizationPasswordUser = document.querySelector(".authorization-password-input"); 
+const authorizationButton = document.querySelector(".authorization-button"); 
+const authorizationForm = document.querySelector(".authorization-form");
+const authorizationSection = document.querySelector(".authorization-section"); 
 
 
 let authorizationIsStorageSupport = true;
 
 try {
-    storageEmail = localStorage.getItem('email');
+    storageEmail = localStorage.getItem("email");
 } catch (err) {
     authorizationIsStorageSupport = false;
 }
 
-authorizationForm.addEventListener('submit', function (evt) {
+authorizationForm.addEventListener("submit", function (evt) {
     if (!authorizationEmailUser.value || ! authorizationPasswordUser.value) {
         evt.preventDefault();
-        authorizationSection.classList.remove('feedback-error');
+        authorizationSection.classList.remove("feedback-error");
         authorizationSection.offsetWidth = authorizationSection.offsetWidth;
-        authorizationSection.classList.add('feedback-error');
+        authorizationSection.classList.add("feedback-error");
     } else {
         if (authorizationIsStorageSupport) {
-            localStorage.setItem('email', authorizationEmailUser.value);
+            localStorage.setItem("email", authorizationEmailUser.value);
         }
     }
 });
---
+++ js/radio_min.js
@@ -0,0 +1,1 @@
+const fatFilterItemList=document.querySelectorAll(".fat-content-filter-item");fatFilterItemList.forEach(el=>el.childNodes[3].addEventListener('keydown',function(event){if(event.keyCode==13)el.childNodes[1].checked='checked'}));
