c186d0e174a72caf9aacf68ad0c760371b86ce3e
maremshaowa.nasti@mail.ru <maremshaowa.nasti@mail.ru>
2021-03-18 02:44:00 +0300

    Исправление замечаний насчет checkbox и radio

--- js/radio.js
+++
@@ -1,8 +0,0 @@
-const fatFilterItemList = document.querySelectorAll(".fat-content-filter-item");
-
-fatFilterItemList.forEach(el =>
-    el.childNodes[3].addEventListener('keydown', function(event) {
-        if (event.keyCode == 13)
-        el.childNodes[1].checked = true;
-    })
-); 
